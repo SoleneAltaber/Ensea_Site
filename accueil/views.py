323c0f3f5c1c61d5293f2eb204bcacdfd4c56046
SoleneAltaber <solene.altaber@orange.fr>
2022-02-01 15:00:27 +0100

    ca marche vitF

--- accueil/views.py
+++ accueil/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import datetime
+
 
 def post_list(request):
     return render(request, 'blog/post_list.html', {})
+ 
+
+def current_datetime(request):
+    now = datetime.datetime.now()
+    html = "<html><body>It is now %s.</body></html>" % now
+    return HttpResponse(html)
+    
     
     
     
